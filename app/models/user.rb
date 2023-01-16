3a08368a78266ab16f92dea12a6f12a39cd0f7f9
tsekhanskaya <anutta2222@gmail.com>
2023-01-16 16:36:40 +0100

    Added messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
   has_many :rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
 end
