7a7bb1467834a5b3143ce6c2457765820bd926de
tsekhanskaya <anutta2222@gmail.com>
2023-01-16 16:01:45 +0100

    Added model room

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable
-  # :rememberable
+  has_many :rooms, dependent: :destroy
 end
